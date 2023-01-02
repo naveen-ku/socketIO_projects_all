7a407a8707a11f9d1fdce7c7bd220d783fcb4e75
naveen-ku <naveen2951996@gmail.com>
2023-01-02 20:57:47 +0530

    adds: socketio_demo remove cors error

--- socketio_demo/socketioServer.js
+++ socketio_demo/socketioServer.js
@@ -1,14 +1,19 @@
 const http = require("http");
 const socketio = require("socket.io");
+const cors = require("cors");
 
 const server = http.createServer((req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
     res.end("I am connected !!");
     console.log("Server running");
 });
 
 
-const io = socketio(server);
+const io = socketio(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
 io.on('connection', (socket, req) => {
     socket.emit('welcome',"Welcome to socketio server !!!")
